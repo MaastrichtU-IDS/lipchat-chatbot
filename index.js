55c4b04e524dee3060f5073024c9655d3fdae91f
Kody Moodley <kody.moodley@gmail.com>
2019-01-18 15:29:45 +0100

    maintenance issues

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//var backend_url_w2v = "https://chatbot.zapto.org/w2v";
-//var backend_url_ont = "https://chatbot.zapto.org/ontology";
-//var backend_url_qa = "https://chatbot.zapto.org/qa";
+//var backend_url_w2v = "http://lipchat.dumontierlab.com/w2v";
+//var backend_url_ont = "https://lipchat.dumontierlab.com/ontology";
+//var backend_url_qa = "https://lipchat.dumontierlab.com/qa";
 var backend_url_w2v = "http://127.0.0.1:5566";
 var backend_url_ont = "http://127.0.0.1:5577";
 var backend_url_qa = "http://127.0.0.1:5588";
 var using_ontology = false;
-// var using_ontology = true;
 var isEnglishInput = false;
 var ontology_support = [];
 var ontology_response = '';
@@ -13,7 +12,70 @@ var ontology_same_response_count = 0;
 var chat_id = 0;
 var qa_index = 0;
 var qa_data = [];
+var response_counter = 0;
 var number_of_trys = 0
+var use = 0;  // 0 - baduse, 1 - wear and tear or natural disaster
+var object = 0; // 0 - bigobject, 1 - smallobject
+var cost = 0; // 0 - highcost, 1 - lowcost
+var smallobjects = [
+"cable",
+"chair",
+"couch",
+"doorknob",
+"doorbell",
+"doorstep",
+"switch",
+"spare",
+"part",
+"handrail",
+"key",
+"lamp",
+"light",
+"mailbox",
+"letterbox",
+"shutter",
+"table",
+"socket",
+"hinge"
+]
+
+var bigobjects = [
+"bath",
+"bed",
+"bookshelf",
+"cabinet",
+"carpet",
+"ceiling",
+"central heating",
+"chimney",
+"cooker",
+"stove",
+"cupboard",
+"garbage",
+"floor",
+"drain",
+"dryer",
+"electrical",
+"faucet",
+"tap",
+"gutter",
+"heating",
+"internet",
+"lock",
+"oven",
+"refrigerator",
+"roof",
+"septic",
+"toilet",
+"sink",
+"stove",
+"tv",
+"ventilation",
+"wall",
+"washing",
+"window"
+]
+
 
 function getBotResponse() {
   var rawText = $("#textInput").val();
@@ -89,7 +151,7 @@ function getQaResponse(text) {
 }
 function getWatsonResponse(text) {
   //console.log("kody9");
-  console.log(text);
+  //console.log(text);
   $.ajax({ 
     // url: '/frontend/index.php',
     url: 'index.php',
@@ -116,7 +178,7 @@ function getWatsonResponse(text) {
     },
     error: function (err) {
       //console.log("kody12");
-      console.log(err);
+      //console.log(err);
 
       // appendToChat(true, 'Sorry, I can not help you with that');
       // $(".typing").css("display", "none");
@@ -129,9 +191,157 @@ function getWatsonResponse(text) {
     }
   });
 }
+
+
+
+
+
+function displayResponse(txt) {
+  $(".typing").css("display", "block");
+    setTimeout(function(){
+      $(".typing").css("display", "none");
+      appendToChat(true, txt);
+    },800);
+}
+
+
+
+
+
+
+
 function getOntologyResponse(text) {
-  $.get(backend_url_ont + "/get", { msg: text, token: chat_id }).done(function(data) {
-    if (data.ask) {
+  var lowercaseStr = text.toLowerCase();
+  if (response_counter == '0'){
+    displayResponse('Your issue seems to be about a property maintenance or property damage issue, is this correct?');
+    response_counter++;
+  }
+  else if (response_counter == '1'){
+    if (lowercaseStr.includes("yes") || lowercaseStr == 'y' || lowercaseStr == 'ya' || lowercaseStr.includes("yeah") || lowercaseStr == 'yep'){
+      displayResponse('Okay, what item of property was damaged or needs maintenance?');
+      response_counter++;
+    }
+    else if ((lowercaseStr[0] == 'n' && lowercaseStr[1] == 'o') || lowercaseStr == 'n' || lowercaseStr == 'nah' || lowercaseStr.includes("nope") || lowercaseStr == 'nay' || lowercaseStr.includes("nope")){
+      displayResponse('Okay, it seems your issue is not related to property damage or maintenance and I do not have enough knowledge to answer it right now. Perhaps I will in the near future! If I can assist with any other issue, you are welcome to mention it now. Otherwise, take care and goodbye!');
+      response_counter = 0; 
+      using_ontology = false;
+      
+    } 
+    else{
+      displayResponse('Hmmm. I do not understand that. I need a yes or no response.');  
+      response_counter = 1;
+      
+    } 
+  }
+  else if (response_counter == '2'){  // object
+    var foundBig = false;
+    var foundSmall = false;
+    var item = '';
+    for (i = 0; i < bigobjects.length; i++) {
+      if (lowercaseStr.indexOf(bigobjects[i]) !== -1){
+        item = bigobjects[i];
+        lowercaseStr = "big";
+        foundBig = true;
+        break;
+      }
+    }
+
+    if (!foundBig){
+      for (i = 0; i < smallobjects.length; i++) {
+        if (lowercaseStr.indexOf(smallobjects[i]) !== -1){
+          item = smallobjects[i];
+          lowercaseStr = "small";
+          foundSmall = true;
+          break;
+        }
+      }
+    }
+
+    thing = JSON.stringify(item);
+    thing = thing.replace(/^"(.*)"$/, '$1');
+    if (lowercaseStr == 'big'){
+      displayResponse("If your " + thing + " was damaged, could it have been caused through negligence or irregular maintenance on your part (yes / no)?");
+      object = 0;
+      response_counter++;
+      
+    }
+    else if (lowercaseStr == 'small'){
+      displayResponse("If your " + thing + " was damaged, could it have been caused through negligence or irregular maintenance on your part (yes / no)?");
+      object = 1;
+      response_counter++;
+      
+    }
+    else { 
+      displayResponse('Hmmm. I am sorry, I do not recognize such an object. Perhaps I will in the near future! If I can assist with any other issue, you are welcome to mention it now. Otherwise, take care and goodbye!');  
+      response_counter = 0; 
+      using_ontology = false;
+      
+    }
+  }
+  else if (response_counter == '3'){  // use
+    if (lowercaseStr.includes("yes") || lowercaseStr == 'y' || lowercaseStr == 'ya' || lowercaseStr.includes("yeah") || lowercaseStr == 'yep'){
+      displayResponse('Can you give an estimate in EURO how much it might cost to address your ' + thing + ' issue?');
+      use = 0;
+      response_counter++;
+      
+    }
+    else if ((lowercaseStr[0] == 'n' && lowercaseStr[1] == 'o') || lowercaseStr == 'n' || lowercaseStr == 'nah' || lowercaseStr.includes("nope") || lowercaseStr == 'nay' || lowercaseStr.includes("nope")){
+      displayResponse('Can you give an estimate in EURO how much it might cost to address your ' + thing + ' issue?');
+      use = 1;
+      response_counter++;       
+      
+    } 
+    else{
+      displayResponse('Hmmm. I do not understand that. I need a yes or no response.');
+      
+    } 
+  }
+  else if (response_counter == '4'){  // object response
+    var numStr= lowercaseStr.replace( /[^\d\.]*/g, '');
+
+    if (lowercaseStr.includes("yes") || lowercaseStr == 'y' || lowercaseStr == 'ya' || lowercaseStr.includes("yeah") || lowercaseStr == 'yep'){
+      displayResponse('How much?');
+      //response_counter++;       
+      
+    }
+    else if (numStr.length != 0){
+      var num = parseFloat(numStr);
+      if (num >= 80.00){
+        cost = 0;
+      }
+      else{
+        cost = 1;
+      }
+      
+      $.get(backend_url_ont + "/get", { usage: use, propertyitem: object, actioncost: cost, token: chat_id }).done(function(data) {
+        if (data.text){
+          var html = '<li class="sent sent-multi" style="margin-top: 15px;"><img src="resources/images/robot512x512.png" alt=""/><p>' + data.text + '</p></li>';
+          $("#chatbox").append(html);
+          
+        }
+        $(".typing").css("display", "none");        
+      });
+
+      response_counter = 0; 
+      using_ontology = false;
+      
+    } 
+    else{
+      displayResponse('Hmmm. I am sorry, unfortunately I do not have enough information to help with this issue. If I can assist with any other issue, you are welcome to mention it now. Otherwise, take care and goodbye!');  
+      response_counter = 0; 
+      using_ontology = false;
+      
+    }  
+
+  }
+
+/*  $.get(backend_url_ont + "/get", { msg: text, token: chat_id, respcount: response_counter }).done(function(data) {
+    appendToChat(true, data.ask[ontology_property]);
+    console.log(data);
+    if response_c
+    if (data.ask) { // chatbot question is sent back
+      console.log(Object.keys(data.ask)[0]);
+      console.log(data.ask[ontology_property]);
       var ontology_property = Object.keys(data.ask)[0];
       ontology_support = [];
       if (ontology_response === data.ask[ontology_property]) {
@@ -151,7 +361,7 @@ function getOntologyResponse(text) {
         ontology_response = data.ask[ontology_property];
         appendToChat(true, data.ask[ontology_property]);
       }
-    } else if (data.text) {
+    } else if (data.text) { // chatbot response or answer is sent back
       if (data.support) {
         ontology_support = data.support;
         var html = '<li class="sent sent-multi" style="margin-top: 15px;"><img src="resources/images/robot512x512.png" alt=""/><p>' + data.text + '</p></li>';
@@ -168,7 +378,7 @@ function getOntologyResponse(text) {
         appendToChat(true, data.text);
       }
     } else {
-      if (number_of_trys < 3){
+      /*if (number_of_trys < 3){
         appendToChat(true, 'Sorry, I do not understand...')
       }else {
         appendToChat(true, 'Sorry, I could not be of assistance. Could you try agin?');
@@ -185,8 +395,21 @@ function getOntologyResponse(text) {
       number_of_trys++;
     }
     $(".typing").css("display", "none");
-  });
+  });*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 function isEnglish(text, callback) {
   if (isEnglishInput || text.length < 20) {callback(true);}
   else {
